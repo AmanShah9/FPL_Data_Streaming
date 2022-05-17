74ad18361f06588ec46671924a5bb9784485b0f6
AmanShah9 <aman.17bec1034@abes.ac.in>
2022-05-17 23:35:38 +0530

    First Commit

--- Extract.py
+++ Extract.py
@@ -3,8 +3,8 @@ import json
 from pprint import pprint
 
 
-main_url = 'https://fantasy.premierleague.com/api/'
+base_url = 'https://fantasy.premierleague.com/api/'
 
-r = requests.get(main_url + 'bootstrap-static/').json()
+r = requests.get(base_url+'bootstrap-static/').json()
 
 pprint(r, indent=2, depth=1, compact=True)
